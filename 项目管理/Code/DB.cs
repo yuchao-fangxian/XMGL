7571f411911d0d693a2293de22c6a92076d1d239
余超 <xa87@live.cn>
2017-12-09 09:05:19 +0800

    添加项目文件。

---
+++ 项目管理/Code/DB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace 项目管理.Code
+{
+    /// <summary>
+    /// 数据库类
+    /// </summary>
+    public class DB
+    {
+        public string conn_str = "Data Source=192.168.1.97;Initial Catalog=AIS20130108175955;User ID=sa;";
+
+    }
+}
